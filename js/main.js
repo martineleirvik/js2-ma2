469ecaa0952c80a9b36cf9d6ea9f85490fcf343a
martineleirvik <leirvikmartine@gmail.com>
2021-12-05 20:36:57 +0100

    D

--- js/main.js
+++ js/main.js
@@ -1,13 +1,45 @@
 import { books } from "./components/data.js";
 
-const booksContainer = document.querySelector("ul");
 
-books.forEach(function(item)  {
-    console.log(item);
+function createList() {
 
-    booksContainer.innerHTML += `<div class="card">
-                                <li><span> Title: ${item.title}</span></li>
-                                <li><span> ISBN: ${item.isbn}</span></li>
-                                </div>`;
+    const booksContainer = document.querySelector("ul");
 
-});
+    books.forEach(function(item)  {
+        console.log(item);
+
+        booksContainer.innerHTML += `<div class="card">
+                                    <li><span> Title: ${item.title} </span></li>
+                                    <li><span> ISBN: ${item.isbn}</span></li>
+                                    <li><span> <i class="fa fa-trash" data-item="${item.title}"></i> </span></li>
+                                    </div>`;
+
+    });
+
+    const trashCan = document.querySelectorAll("li i");
+
+    trashCan.forEach(function(can){
+        can.addEventListener("click", removeFromList);
+    });
+
+};
+
+createList();
+
+
+function removeFromList() {
+    console.log(event);
+
+    const deleteItem = event.target.dataset.item.title;
+
+    const newList = books.filter(function(item){
+        if(deleteItem !== item) {
+            return true;
+        }
+    })
+
+    books = newList;
+
+    createList();
+
+}
